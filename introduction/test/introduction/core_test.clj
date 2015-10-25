1733617ba21f9227e3c18a9199ee74e87eeaee0a
James Anthony Lowenthal <loweja01@gmail.com>
2015-10-25 16:24:58 -0400

    Add assertion descriptions to tests

--- introduction/test/introduction/core_test.clj
+++ introduction/test/introduction/core_test.clj
@@ -1,12 +1,18 @@
 (ns introduction.core-test
   (:require [clojure.test :refer :all]
             [introduction.core :refer :all])
-  (clojure.core/load "fp-filter-array"))
+  (load "fp-filter-array")
+  (load "fp-hello-world"))
+
+;;fp-hello-world
+(deftest my-fp-hello-world
+  (is
+    (= (with-out-str (introduction.core/fp-hello-world))
+       "Hello World\n" ) "Should print out Hello World\n"))
 
 ;;fp-filter-array
-
 (deftest my-fp-filter-array
   (is 
     (= 
       (introduction.core/fp-filter-array 3 '(10 9 8 2 7 5 1 3 0))
-      '(2 1 0))))
+      '(2 1 0)) "Should return a vector containing all the numbers in the list less than the delimiter in original order"))
