fac753026ff50a1d7f569e710c9e8f43050a6b6a
James Anthony Lowenthal <loweja01@gmail.com>
2015-10-25 15:25:54 -0400

    Add debugger to project.clj

--- introduction/project.clj
+++ introduction/project.clj
@@ -1,6 +1,7 @@
 (defproject introduction "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "answers to introductory questions from HackerRank"
+  :url "https://github.com/JamesAnthonyLow/hacker-rank-fp-challenges-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [debugger "0.1.7"]])
